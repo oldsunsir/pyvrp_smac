673ea1cc9ff23733b8f0ee5c136f19a34aa0a487
ruofei sun <bhsrf2019@126.com>
2024-04-21 15:21:59 +0800

    添加清空instace_res_of_param操作，并规范命名

--- pap.py
+++ pap.py
@@ -9,10 +9,10 @@ class PAP:
     self.instances : 所有样例路径
     self.algos : 算法集合
     self.instance2algo : 样例对应的最佳算法与结果, 其中key为CVRPTarge实例, val是对应算法参数字典
-    self.instancesResOfParam : 每个测试的param对应的所有instance结果, 方便后续处理instance2algo中cvrptarge.cost 与 dict
+    self.instance_res_of_param : 每个测试的param对应的所有instance结果, 方便后续处理instance2algo中cvrptarge.cost 与 dict
                                 注意需要通过回调函数即时清除, 比如我要每10个算法选一个最好的, 那就每评估10个之后清除
     """
-    def __init__(self, folder_path) -> None:
+    def __init__(self, folder_path : str, iteration : int = 100) -> None:
         self.instances = []
 
         instances_path = os.listdir(folder_path)
@@ -23,10 +23,10 @@ class PAP:
                 self.instances.append(instance_path)
 
         ## 初始每个instance对应一个空算法
-        self.instance2algo = {CVRPTarget(file_path = instance) : dict() for instance in self.instances}
+        self.instance2algo = {CVRPTarget(file_path = instance, iterations = iteration) : dict() for instance in self.instances}
 
         self.algos : list[dict] = []
-        self.instancesResOfParam : dict = {}
+        self.instance_res_of_param : dict = {}
 
     def papTarget(self, param : params) -> float:
         """
@@ -35,34 +35,37 @@ class PAP:
             针对某个样例, 对比instance2algo中记录的结果与当前param计算结果, 
             取最小值
             结果依然是越小越好
-        注意需要更新self.instancesResOfParam
+        注意需要更新self.instance_res_of_param
         """
         sum = 0
-        self.instancesResOfParam[param] = {}
+        self.instance_res_of_param[param] = {}
         for cvrp_target, _ in self.instance2algo.items():
             cur_res = cvrp_target.getCost(cvrp_params = param)
-            self.instancesResOfParam[param][cvrp_target] = cur_res
+            self.instance_res_of_param[param][cvrp_target] = cur_res
             sum += min(cur_res, cvrp_target.cost)
         return sum
 
-    def papUpdate(self, bestParam : params):
+    def papUpdate(self, best_param : params):
         """
         从每n个算法中, 选出最佳参数配置的算法后, 即时更新instance2algo与algos
         """
-        self.algos.append(bestParam)
-        bestDictOfParam = bestParam.to_dict
-        for cvrp_target, _ in self.instance2algo.items():
-            assert bestParam in self.instancesResOfParam.keys()
-            if self.instancesResOfParam[bestParam][cvrp_target] < cvrp_target.cost:
-                cvrp_target.cost = self.instancesResOfParam[bestParam][cvrp_target]
-                self.instance2algo[cvrp_target] = bestDictOfParam
-
+        self.algos.append(best_param)
+        best_dict_of_param = best_param.to_dict
+        assert best_param in self.instance_res_of_param.keys()
+        for cvrp_target in self.instance2algo.keys():
+            if self.instance_res_of_param[best_param][cvrp_target] < cvrp_target.cost:
+                cvrp_target.cost = self.instance_res_of_param[best_param][cvrp_target]
+                self.instance2algo[cvrp_target] = best_dict_of_param
+        ##每个param更新过之后就不再用到
+        self.instance_res_of_param.clear()
         
 
 if __name__ == "__main__":
     tmp_pap = PAP(folder_path = "CVRP_Data")
     tmp_param = params.get_initial_params(path = "cvrp.toml")
-    print(tmp_pap.papTarget(param = tmp_param))
-
-    tmp_pap.papUpdate(bestParam = tmp_param)
+    tmp_pap.papTarget(param = tmp_param)
+    tmp_pap.papUpdate(best_param = tmp_param)
+    for cvrp_target, dict1 in tmp_pap.instance2algo.items():
+        print(f"{cvrp_target.instance_path} 当前结果为 {cvrp_target.cost}")
+        print(dict1)
     
