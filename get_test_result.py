54f6e6fa831483e5bc498e96ac61c1061fe63291
bobsun <bhsrf2019@126.com>
2024-05-06 15:20:10 +0800

    添加测试结果记录,fix get_test_result.py bug

--- get_test_result.py
+++ get_test_result.py
@@ -17,12 +17,12 @@ with open("record.txt", 'r', encoding="utf-8") as f:
             config_dict = ast.literal_eval(config_text)
             test_pap.append(params(**(config_dict)))
 
-folder_path = "tmp_Data"
+folder_path = "Test_MDVRP_Data"
 instances_path = os.listdir(folder_path)
 # 遍历文件夹下的所有文件和文件夹
 for item in instances_path:
     instance_path = os.path.join(folder_path, item)
-    single_vrp_target = CVRPTarget(file_path = instance_path)
+    single_vrp_target = MDVRPTarget(file_path = instance_path)
     best_res = float("inf")
     best_idx = -1
     for i, config in enumerate(test_pap):
@@ -31,9 +31,9 @@ for item in instances_path:
                                    population = config.pop_params,
                                    neighbourhood = config.nb_params)
         tmp_cost = single_vrp_target.model.solve(params = solve_params, 
-                    stop = MaxRuntime(max_runtime = 10), seed = 42, display = False).cost()
+                    stop = MaxRuntime(max_runtime = 30), seed = 42, display = False).cost()
         if tmp_cost < best_res:
             best_res = tmp_cost
             best_idx = i
     print(f"best res for {instance_path} : {best_res}\
-            best config : {config.to_dict}")
+            best config : {test_pap[best_idx].to_dict}")
