c240225f913da589c52c13e666f60131843efbea
bobsun <bhsrf2019@126.com>
2024-05-06 10:06:13 +0800

    添加test_cvrp_data测试结果

--- get_test_result.py
+++ get_test_result.py
@@ -17,7 +17,7 @@ with open("record.txt", 'r', encoding="utf-8") as f:
             config_dict = ast.literal_eval(config_text)
             test_pap.append(params(**(config_dict)))
 
-folder_path = "tmp_Data"
+folder_path = "Test_CVRP_Data"
 instances_path = os.listdir(folder_path)
 # 遍历文件夹下的所有文件和文件夹
 for item in instances_path:
@@ -31,9 +31,9 @@ for item in instances_path:
                                    population = config.pop_params,
                                    neighbourhood = config.nb_params)
         tmp_cost = single_vrp_target.model.solve(params = solve_params, 
-                    stop = MaxRuntime(max_runtime = 10), seed = 42, display = False).cost()
+                    stop = MaxRuntime(max_runtime = 30), seed = 42, display = False).cost()
         if tmp_cost < best_res:
             best_res = tmp_cost
             best_idx = i
     print(f"best res for {instance_path} : {best_res}\
-            best config : {config.to_dict}")
+            best config : {test_pap[i].to_dict}\n")
